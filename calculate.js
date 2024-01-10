4c5d9799b9aeadb36c972a89db5c09b21b779aea
Elias Aitovaara <elias.aitovaara@gmail.com>
2024-01-10 18:19:49 +0200

    media query and styling

--- calculate.js
+++ calculate.js
@@ -3,7 +3,7 @@ const calcButton = document.querySelector(".calc-button");
 calcButton.addEventListener("click", calculateAge);
 
 function calculateAge() {
-  // Fetching values from the html input fields
+  // Fetching values from the HTML input fields
   const day = parseInt(document.getElementById("day").value);
   const month = parseInt(document.getElementById("month").value);
   const year = parseInt(document.getElementById("year").value);
@@ -11,20 +11,20 @@ function calculateAge() {
   // Creating date from the input fields
   const birthDate = new Date(year, month - 1, day);
 
-  // present date
+  // Present date
   const presentDate = new Date();
 
-  // calculating presentdate minus the dates from input fields
+  // Calculating present date minus the dates from input fields
   const timeCalc = presentDate - birthDate;
 
   const ageDate = new Date(timeCalc);
   const years = ageDate.getUTCFullYear() - 1970;
   const months = ageDate.getUTCMonth();
-  const days = ageDate.getUTCDay() - 1;
+  const days = ageDate.getUTCDate() - 1;
 
   const resultsElements = document.getElementById("result");
-  //cannot use " ", Template literals ' '
-  resultsElements.innerHTML = `<span class="colored">${years}</span> years<br>
-                             <span class="colored">${months}</span> months<br>
-                             <span class="colored">${days}</span> days`;
+
+  resultsElements.innerHTML = `${years}<span class="colored"> years</span><br>
+                             ${months}<span class="colored"> months</span><br>
+                             ${days}<span class="colored"> days</span>`;
 }
